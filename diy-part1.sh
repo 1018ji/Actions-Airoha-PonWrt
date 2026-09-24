b3e061ffd4465dc656ca7e06f57740bbe707f67e
yangyang.hou <yangyang.hou@happyelements.com>
2026-09-24 12:47:05 +0800

    添加 luci-app-airoha-npu

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Add the Airoha NPU LuCI application as a local feed.
+mkdir -p ../airoha-feed
+git clone --depth=1 https://github.com/rchen14b/luci-app-airoha-npu.git ../airoha-feed/luci-app-airoha-npu || exit 1
+rm -rf ../airoha-feed/luci-app-airoha-npu/luci-app-airoha-npu
